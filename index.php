ac47aed697d05c384d7b45fad022b26436389f86
benchantech <51009104+benchantech@users.noreply.github.com>
2025-05-12 17:52:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:url" content="https://gleam.io/18NWm/first-50-patreon-members-only-3month-40-off"/>
+    <meta property="og:title" content=""/>
+    <meta property="twitter:card" content="summary"/>
+    <meta property="fb:app_id" content="152351391599356"/>
+    <meta property="og:description" content=""/>
+
+    <style>
+      iframe{width:100%; height: 580px; overflow: hidden}
+    </style>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162089981-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-162089981-1');
+</script>
+  </head>
+  <body>
+<?php
+echo '<br />';
+echo 'Right click and choose Save As<br /><br />';
+echo '<div style="float: left; padding: 0 20px 0 20px">';
+?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- benchanmusic sidebar -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:120px;height:600px"
+	          data-ad-client="ca-pub-6543946757233630"
+		       data-ad-slot="5579322418"></ins>
+		       <script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<?php
+echo '</div>';
+echo '<div style="float: left">';
+?>
+<!--iframe src="https://gleam.io/18NWm/first-50-patreon-members-only-3month-40-off" frameBorder="0" allowfullscreen></iframe>
+<br /><br /-->
+<?php
+//foreach(glob('*', GLOB_ONLYDIR) as $e) {
+	//echo $e.'<br />';
+	foreach(glob('*.*') as $f) {
+		if ($f != 'index.php') {
+			echo '<a href="'.$f.'">'.$f.'</a><br />';
+		}
+	}
+//}
+echo '</div>';
+?>
+</body>
+</html>
+<?php
